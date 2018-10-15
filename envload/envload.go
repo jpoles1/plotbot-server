4cc0a1847aae08c819e2cfce56fbef0eaec4274d
Jordan Poles <jpoles1@gmail.com>
2018-10-15 18:03:38 -0400

    Initial Commit

---
+++ envload/envload.go
@@ -0,0 +1,60 @@
+package envload
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"virtualpatient/logging"
+
+	"github.com/fatih/color"
+	"github.com/subosito/gotenv"
+)
+
+//VirtualPatientConfig is used to describe the configuration state of the Go server
+type VirtualPatientConfig struct {
+	Production bool
+	BindPort   string
+	BindIP     string
+}
+
+func loadBoolEnv(varName string) bool {
+	if os.Getenv(varName) == "" {
+		color.Yellow(fmt.Sprintf("Missing %s value in .env file, automatically setting to false.\nSet a boolean value for %s in your .env file to disable this warning.", varName, varName))
+		return false
+	}
+	boolEnv, err := strconv.ParseBool(os.Getenv(varName))
+	if err != nil {
+		color.Yellow(fmt.Sprintf("%s value must be a valid bool (true or false)\n Automatically setting to false.", varName))
+		return false
+	}
+	return boolEnv
+
+}
+
+func loadStringEnv(varName string) string {
+	if os.Getenv(varName) == "" {
+		logging.Fatal(fmt.Sprintf("Missing %s value in .env file.", varName), errors.New("No such string var defined"))
+	}
+	return os.Getenv(varName)
+}
+
+//LoadEnv uses gotenv to pull in the env files specified in the args, and returns a config struct
+func LoadEnv(filenames ...string) (VirtualPatientConfig, error) {
+	//Load Env
+	err := gotenv.Load(filenames...)
+	if err != nil {
+		logging.Error("Error loading .env file: ", err)
+		return VirtualPatientConfig{}, err
+	}
+	//Setup global env variables
+	config := VirtualPatientConfig{
+		Production: loadBoolEnv("PRODUCTION"),
+		BindPort:   loadStringEnv("BIND_PORT"),
+		BindIP:     loadStringEnv("BIND_IP"),
+		/*BindURL:    loadStringEnv("BIND_URL"),
+		JWTSecret:  loadStringEnv("JWT_SECRET"),
+		MongoURI:   loadStringEnv("MONGO_URI"),*/
+	}
+	return config, nil
+}
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"plotbot-server/envload"
+
+	"github.com/fatih/color"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/gorilla/websocket"
+)
+
+type stepperCommand struct {
+	Degrees   int  `json:"deg"`
+	Clockwise bool `json:"dir"`
+}
+
+type plotCommand struct {
+	LeftStepper  stepperCommand `json:"left"`
+	RightStepper stepperCommand `json:"right"`
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	envData, err := envload.LoadEnv(".env")
+	if err != nil {
+		log.Fatal("Failed to load environment config.")
+	}
+	router := chi.NewRouter()
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+	router.Route("/", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("test response"))
+		})
+	})
+	router.Route("/api", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+			for {
+				// Read message from browser
+				msgType, msg, err := conn.ReadMessage()
+				if err != nil {
+					return
+				}
+
+				// Print the message to the console
+				fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+				// Write message back to browser
+				if err = conn.WriteMessage(msgType, msg); err != nil {
+					return
+				}
+			}
+		})
+	})
+	color.Green("Starting web server at: http://%s:%s", envData.BindIP, envData.BindPort)
+	log.Fatal(http.ListenAndServe(envData.BindIP+":"+envData.BindPort, router))
+}
